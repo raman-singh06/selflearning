1ae17bdf37d2c25f6994feb816b252a49fadb350
raman-singh06 <you@example.com>
2024-03-08 10:57:57 -0800

    new changes

--- javascript/test2.js
+++ javascript/test2.js
@@ -1,4 +1,4 @@
-console.log("heelo");
+//console.log("heelo");
 
 //this change is done my Raman2
 //this change is done my raman1
@@ -6,3 +6,24 @@ console.log("heelo");
 //this change is done my raman1
 // make a change
 // another change made
+
+function foo(agr1, arg2){
+    if(agr1.length == 0) {
+        console.log("array cannot be empty")
+    } 
+    else {
+        var arr =agr1[0]+arg2[0];
+        // console.log(arg1.shift())
+        console.log(arr);
+    }
+}
+
+arr1 = [1,2,3,4,5]
+arr2 = [2,4]
+foo(arr1, arr2)
+
+
+
+
+
+
