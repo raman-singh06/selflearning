ae8d92761db867497acd7b5cc8c3a94a776cd10d
raman-singh06 <you@example.com>
2024-03-14 22:50:10 -0700

    new change

--- javascript/test.js
+++ javascript/test.js
@@ -1,3 +1,21 @@
-const myArray = [50, 60, 70];
-const myData = myArray[0];
-console.log(myData);
+function foo (arg1, arg2){
+    var result = [];
+    result = arg1.push(...arg2);
+    return arg1;
+}
+//arg1 =["hello", 1];
+//arg2 =[" hi", 2];
+var result = foo(["hello", 1], [" hi", 2]);
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+// write a function that accepts 2 arg - arg1 and arg 2. arg is an array and arg 2 is also an array. your function should add these array an arry that has elements for
